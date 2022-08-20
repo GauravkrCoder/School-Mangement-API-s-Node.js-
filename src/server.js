a9036977d273d91cb7b0c10d78b14f2c46cb85c1
Gaurav Kumar <Gaurav.Kumar@matellio.com>
2022-08-20 11:45:10 +0530

    Updated Code

--- src/server.js
+++ src/server.js
@@ -1,25 +1,38 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-require('dotenv').config();
-var client = require('./config/postgresDB.config');
+require("dotenv").config();
+var client = require("./config/postgresDB.config");
+
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With"
+  );
+  res.header("Access-Control-Allow-Methods", "*");
+  return next();
+});
 
 /* importing different routes */
-var userRoute = require('./routes/userRoute');
+var userRoute = require("./routes/userRoute");
 
-app.use('/api/user', userRoute.router);
+app.use("/api/user", userRoute.router);
 
-
-
-app.get('/', (request, response) => {
+app.get("/", (request, response) => {
   // client.connect();
-  client.query('select * from _tblSuperSurveyUsers', (err, result) => {
+  client.query("select * from _tblSuperSurveyUsers", (err, result) => {
     if (err) {
-      console.log(err.stack)
+      console.log(err.stack);
     } else {
-      response.send(result.rows)
+      // response.send(result.rows);
+      response.status(200).json({
+        code: 200,
+        message: "Get User List Successfully.",
+        data: result.rows,
+      });
       // client.end();
     }
-  }) 
+  });
 });
 
 /* Setting the PORT NUMBER */
@@ -28,4 +41,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, (err) => {
   if (err) throw err;
   console.log(`Server is running on port http://localhost:${PORT}`);
-})
+});
